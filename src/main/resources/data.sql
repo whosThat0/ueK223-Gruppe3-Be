05de890f1713f4060e52ea9d28059bdaf9b18b32
hacker5ele <eleonora.vynogradova@edu.tbz.ch>
2025-09-24 15:57:38 +0200

    delete useless roles for admin

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -20,7 +20,7 @@ ON CONFLICT DO NOTHING;
 --AUTHORITIES
 INSERT INTO authority(id, name)
 VALUES ('2ebf301e-6c61-4076-98e3-2a38b31daf86', 'DEFAULT'),
-('76d2cbf6-5845-470e-ad5f-2edb9e09a868', '  USER_MODIFY'),
+('76d2cbf6-5845-470e-ad5f-2edb9e09a868', 'USER_MODIFY'),
 ('21c942db-a275-43f8-bdd6-d048c21bf5ab', 'USER_DEACTIVATE'),
 ('e7c3f0a8-1234-4a56-8901-abcdef123456', 'GROUP_CREATE'),
 ('abcd1234-5678-90ef-1234-567890abcdef', 'ADMIN')
@@ -29,8 +29,6 @@ ON CONFLICT DO NOTHING;
 -- assign roles to users
 INSERT INTO users_role (users_id, role_id)
 VALUES ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'ab505c92-7280-49fd-a7de-258e618df074'),
-    ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'c6aee32d-8c35-4481-8b3e-a876a39b0c02'),
-    ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'd29e709c-0ff1-4f4c-a7ef-09f656c390f1'),
     ('0d8fa44c-54fd-4cd0-ace9-2a7da57992de', 'c6aee32d-8c35-4481-8b3e-a876a39b0c02'), -- user@example.com → USER
     ('0d8fa34c-54fd-4cd0-ace9-2a7da57992de', 'c6aee32d-8c35-4481-8b3e-a876a39b0c02'),  -- extrauser@example.com → USER
 ('0d8fa44c-54fd-4cd0-ace9-2a7da57992de', 'd29e709c-0ff1-4f4c-a7ef-09f656c390f1')
