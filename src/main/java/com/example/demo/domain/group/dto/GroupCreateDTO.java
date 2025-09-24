ee0b75807a53200e7c3bb3cebf3f2be96a0a29b0
Abigail <abigail.llanque@edu.tbz.ch>
2025-09-24 15:37:50 +0200

    Code Quality: NotBlank Message added for Status 400, character between 3 and 100

--- src/main/java/com/example/demo/domain/group/dto/GroupCreateDTO.java
+++ src/main/java/com/example/demo/domain/group/dto/GroupCreateDTO.java
@@ -1,7 +1,7 @@
 package com.example.demo.domain.group.dto;
 
 import com.example.demo.core.generic.AbstractDTO;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -16,11 +16,11 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 public class GroupCreateDTO extends AbstractDTO {
-    @NotNull
+    @NotBlank(message = "Status: 400, Name cannot be empty. Please enter between 3 and 100 characters.")
     @Size(min = 3, max = 100, message = "The group name must be between {min} and {max} characters long.")
     private String name;
     private String motto;
-    @Pattern(regexp = ".*\\.(png|jpg)$", message = "Logo must be a PNG or JPG file")
+    @Pattern(regexp = ".*\\.(png|jpg)$", message = "Logo must be a PNG or JPG file. Uppercase extensions like .PNG or .JPG are not allowed.")
     private String logo;
     private Set<UUID> memberIds;
 }
